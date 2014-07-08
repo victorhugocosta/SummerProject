25eb46f774458229cb31822f7b4a7194a06b1256
Victor Costa <s.victorcosta@gmail.com>
2014-07-08 18:56:13 +0100

    First real commit.

--- m_average.R
+++ m_average.R
@@ -4,8 +4,7 @@ m_average <- function(file, fs=10)
  ro <- length(my.data[,1])
  my.mean <- data.frame(matrix(NA, nrow = ro-fs, ncol = 1)) 
  for (m in seq(1, ro-fs, by=1)){
-  my.mean[m,1] <- mean(my.data[m, 6]:my.data[m+fs-1, 6]) 
-  #skajksjaksja  
+  my.mean[m,1] <- mean(my.data[m, 6]:my.data[m+fs-1, 6])   
   }      
  write.csv(my.mean, "C:/Users/Victor/Dropbox/University of Strathclyde/Summer Project/Data/APD.csv")
  return(my.mean)
